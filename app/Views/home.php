9c202c4316b121812532c2b37d6a1222d53e5172
LydianJay <lloydjayedradan@gmail.com>
2024-09-19 14:44:46 +0800

    initial commit

---
+++ app/Views/home.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lyncxus</title>
+    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <style>
+        :root {
+            --custom-c1: 7, 30, 61;
+            --custom-c2: 31, 66, 135;
+            --custom-c3: 39, 142, 165;
+            --custom-c4: 33, 230, 193;
+            --bs-dark-rgb: var(--custom-c1);
+            --bs-primary-rgb: var(--custom-c3);
+            --bs-secondary-rgb: var(--custom-c2);
+        }
+        .mt-10 {
+            margin-top: 12rem;
+        }
+
+       
+        
+    </style>
+</head>
+<body class="bg-dark">
+    <nav class="navbar bg-secondary">
+        <div class="container-fluid">
+            <a class="navbar-brand text-color-white" href="">
+                <p class="p1 text-white py-0 my-0">Lyncxus</p>
+            </a>
+        </div>
+
+        
+    </nav>
+    <div class="container-fluid mt-10">
+        <div class="d-flex flex-column justify-content-center py-5">
+            <h1 class="h1 text-white text-center my-5">Welcome To Lyncxus Online</h1>
+            <h1 class="h5 text-white text-center mt-5">Website is currently in development</h1>
+            <h1 class="h5 text-white text-center">If you have an idea on what to put here please email me at lydianjayarpilleda@gmail.com</h1>
+            <h1 class="h5 text-white text-center mt-5">Yow wassup - LydianJay</h1>
+            <h1 class="h5 text-white text-center">Tisaaasii!!! - SammyBoy</h1>
+        </div>
+    </div>
+</body>
+</html>
